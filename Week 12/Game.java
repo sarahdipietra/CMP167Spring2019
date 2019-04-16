83a4237c2f8e3cb861deff9b33c9929dbebdea6e
sarahdipietra <47428007+sarahdipietra@users.noreply.github.com>
2019-04-16 12:30:45 -0400

    Add files via upload

---
+++ Week 12/Game.java
@@ -0,0 +1,45 @@
+package Life;
+
+public class Game {
+
+	public static void main(String[] args) {
+		Human personOne = new Human();
+		Human personTwo = new Human();
+		
+		personTwo.personAge = 5;
+		System.out.println("Age of Person One: " + personOne.personAge);
+		System.out.println("Age of Person Two: " + personTwo.personAge);
+
+		/*
+		System.out.println("Hunger: " + personOne.personHungerLevel);
+		System.out.println("Food: " + personOne.personFoodAmount);	
+		
+		System.out.println("Eating Command: ");
+		personOne.eat();
+		System.out.println("Eating Command: ");
+		personOne.eat();
+		System.out.println("Eating Command: ");
+		personOne.eat();
+		
+		System.out.println("Hunger: " + personOne.personHungerLevel);
+		System.out.println("Food: " + personOne.personFoodAmount);	
+		
+		System.out.println("Buying Food Command: ");
+		personOne.buyFood();
+		
+		System.out.println("Working Command: ");
+		
+		System.out.println("Buying Food Command: ");
+		personOne.buyFood();
+		
+		System.out.println("Working Command: ");
+		personOne.work();
+		System.out.println("Working Command: ");
+		personOne.work();
+		System.out.println("Eating Command: ");
+		personOne.eat();
+		System.out.println("Working Command: ");
+		personOne.work();
+		*/
+	}
+}
