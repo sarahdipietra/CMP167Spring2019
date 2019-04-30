d5ac5db4a2f43bd77c0e0b431247c34640ee37bb
sarahdipietra <47428007+sarahdipietra@users.noreply.github.com>
2019-04-30 12:22:00 -0400

    Add files via upload

---
+++ Week 13/Vehicle.java
@@ -0,0 +1,30 @@
+package Life;
+
+public class Vehicle {
+
+	/* Attributes:
+	 * + carColor = String
+	 * + carBrand = String
+	 * + carWheels = int
+	 * Behaviors:
+	 * + start() = void
+	 * + run() = void
+	 * + stop() = void
+	 */
+
+	public static String carColor;
+	public static String carBrand;
+	public static int carWheels;
+
+	public void start() {
+
+	}
+
+	public void run() {
+
+	}
+
+	public void stop() {
+
+	}
+}
