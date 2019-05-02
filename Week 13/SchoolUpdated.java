4e69c56fb53ce301716817a754a87fd558b615ca
sarahdipietra <47428007+sarahdipietra@users.noreply.github.com>
2019-05-02 12:32:49 -0400

    Create SchoolUpdated.java

---
+++ Week 13/SchoolUpdated.java
@@ -0,0 +1,29 @@
+package Life;
+
+public class School {
+
+	/* Attributes:
+	 * + schoolName = String
+	 * + schoolClassroom = String
+	 * + schoolLocation = String
+	 * + schoolIsPublic = boolean
+	 * + schoolIsOpen = boolean
+	 * Behaviors:
+	 * + open() = void
+	 * + close() = void
+	 */
+
+	public static String schoolName;
+	public static String schoolClassroom;
+	public static String schoolLocation;
+	public static boolean schoolIsPublic;
+	public static boolean schoolIsOpen;
+
+	public void open() {
+
+	}
+	
+	public void close() {
+
+	}
+}
